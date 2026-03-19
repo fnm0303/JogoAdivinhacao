17db6e5a270ac8089f13afe5e38bc66c884abef6
Felipe Neto Mello <fnm03br@gmail.com>
2026-03-19 19:52:19 -0300

    Adicionada a estrutura de Input/output do jogo

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+v1
+Iremos fazer um jogo onde o usuário terá chances de acertar um número aleatório decidido pelo sistema.
+
+Input (entrada de dados)
+    O usuário digita número inteiro
+Processamento
+    O sistema compara o número digitado com um número inteiro aleatório
+OutPut (saída de dados)
+    O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do chute.
+*/
+
+Console.WriteLine("---------------------");
+Console.WriteLine("Jogo de Adivinhação");
+Console.WriteLine("---------------------");
+
+Console.WriteLine();
+Console.Write("Digite um número: ");
+string strNumeroDigitado = Console.ReadLine();
+
+Console.WriteLine("O número digitado foi: " + strNumeroDigitado);
+
+Console.ReadLine();
