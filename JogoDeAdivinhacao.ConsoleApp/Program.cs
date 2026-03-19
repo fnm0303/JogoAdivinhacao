3d8917e2b6119ee227986ea61efc8fa2ab9baa9b
Felipe Neto Mello <fnm03br@gmail.com>
2026-03-19 20:51:10 -0300

    Adicionada múltiplas tentativas

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -12,31 +12,48 @@ OutPut (saída de dados)
     O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do chute.
 */
 
-Console.WriteLine("---------------------");
-Console.WriteLine("Jogo de Adivinhação");
-Console.WriteLine("---------------------");
-
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21); //número mínimo, número máximo + 1
+bool jogoDeveContinuar = true;
 
-Console.WriteLine();
-Console.Write("Digite um número: ");
-string strNumeroDigitado = Console.ReadLine();
-
-int numeroDigitado = Convert.ToInt32(strNumeroDigitado);
-// poderia declarar direto int nroAleatorio = Convert.ToInt32(Console.ReadLine());
-if (numeroAleatorio == numeroDigitado) //poderia converter direto nessa linha a string para int
+while (jogoDeveContinuar)
 {
-    Console.WriteLine("Parabéns! Você acertou! O número era " + numeroAleatorio);
-}
+    Console.Clear();
+    Console.WriteLine("---------------------");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("---------------------");
 
-else if (numeroDigitado > numeroAleatorio)
-{
-    Console.WriteLine("O número digitado fo maior que o número secreto!");
-}
+    Console.WriteLine();
+    Console.Write("Digite um número: ");
+    string strNumeroDigitado = Console.ReadLine();
 
-else
-{
-    Console.WriteLine("O número digitado foi menor que o número secreto!");
+    int numeroDigitado = Convert.ToInt32(strNumeroDigitado);
+    // poderia declarar direto int nroAleatorio = Convert.ToInt32(Console.ReadLine());
+    if (numeroAleatorio == numeroDigitado) //poderia converter direto nessa linha a string para int
+    {
+        Console.WriteLine("Parabéns! Você acertou! O número era " + numeroAleatorio);
+        break;
+    }
+
+    else if (numeroDigitado > numeroAleatorio)
+    {
+        Console.WriteLine("O número digitado fo maior que o número secreto!");
+    }
+
+    else
+    {
+        Console.WriteLine("O número digitado foi menor que o número secreto!");
+    }
+
+    Console.WriteLine();
+    Console.Write("Deseja continuar? (s/n)");
+    string opcaoContinuar = Console.ReadLine().ToUpper(); //transforma qualquer letra digitada em maiuscula
+
+    if (opcaoContinuar != "S")
+    {
+        jogoDeveContinuar = false;
+    }
 }
 
 Console.ReadLine();
+
+
